875ff7d466814a7189a25b8641754ffd88963ec8
Lincon-snel <linconsnel@gmail.com>
2022-09-08 23:07:12 +0000

    Using 'write' func to output string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Using 'write' to output a string
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
