ea2cbc7a96ca84118d9db9f28ca85594368a7c83
Lincon-snel <linconsnel@gmail.com>
2022-09-09 20:30:48 +0000

    Using 'putchar' to print out lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
