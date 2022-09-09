a88600c3f5bf5f577e4521e21a5d135d09a69a9a
Lincon-snel <linconsnel@gmail.com>
2022-09-09 22:30:13 +0000

    Printing out figures in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Printing out figures in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", k);
+	}
+	putchar('\n');
+	return (0);
+}
