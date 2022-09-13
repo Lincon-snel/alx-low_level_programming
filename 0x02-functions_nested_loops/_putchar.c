006047b84e6519dd19b7e5c2ec80b3b247a077f4
Lincon-snel <linconsnel@gmail.com>
2022-09-13 18:03:45 +0000

    Printing out '_putchar'

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <unistd.h>
+/**
+ * _putchar - prints char c to standard output
+ *@c: The character to be printed
+ * Description: printing out character with putchar
+ *
+ * Return: 1 (Success)
+ * -1 (Error)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
