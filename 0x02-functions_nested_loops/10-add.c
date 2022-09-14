92547d1ac95c0527ce8018b8eddb1d9851a6641a
Lincon-snel <linconsnel@gmail.com>
2022-09-14 01:52:56 +0000

    Returning the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - to sum two integers
+ * @m: first integer
+ * @n: second integer
+ *
+ * Description: To return the sum of two integers
+ *
+ * Return: result of sum
+ */
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+	return (result);
+}
