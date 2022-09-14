09419d29de9835c3c3585e3f070ae84a8bd94522
Lincon-snel <linconsnel@gmail.com>
2022-09-14 02:39:15 +0000

    Printing all natural numbers between 98 and any given number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: number to start printing from
+ *
+ * Description: To print all natural numbers from
+ * any given value to 98
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+
+			printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
