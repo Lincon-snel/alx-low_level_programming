d9c821a64eeda06f0d27829d10dd5ce0458649b1
Lincon-snel <linconsnel@gmail.com>
2022-09-19 15:56:20 +0000

    Updating variables using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of a variable using pointer
+ * @n: the variable involved
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is for the ALX 0x05-pointers_arrays_strings project under C programming
