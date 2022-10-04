0bb456174f777b2a1ee85a95375f8635902326f8
Lincon-snel <linconsnel@gmail.com>
2022-10-04 15:39:00 +0000

    Concatenation of two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	s3 = malloc((len + 1) * sizeof(char));
+	s3 = malloc(len * sizeof(char));
 
 	if (s3 == NULL)
 		return (NULL);
