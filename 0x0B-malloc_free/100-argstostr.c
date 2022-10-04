41b2b2f5dc482516c4f54a53106b8b40370fcf40
Lincon-snel <linconsnel@gmail.com>
2022-10-04 19:20:47 +0000

    Function concatenates all the arguments of a program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -33,9 +33,13 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
-			str[k++] = av[i][j];
+		{
+			str[k] = av[i][j];
+			k++;
+		}
 
-		str[k++] = '\n';
+		str[k] = '\n';
+		k++;
 	}
 
 	str[k] = '\0';
