bbeb8742ec9f1aebc199587a4f07d40da949bdfb
Lincon-snel <linconsnel@gmail.com>
2022-10-10 15:16:36 +0000

    Prints the current C-file name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the name of current C-file
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
