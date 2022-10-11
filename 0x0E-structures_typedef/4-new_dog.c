218f87e64bbebfb56a89179a7bb4d76561426435
Lincon-snel <linconsnel@gmail.com>
2022-10-11 00:27:59 +0000

    Creating new dog details

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,14 +14,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *copy_name, *copy_owner;
+	int i, len_name = 0, len_owner = 0;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return(NULL);
+		return (NULL);
 
-	dog->name = name;
+	for (i = 0; name[i]; i++)
+		len_name++;
+
+	for (i = 0; owner[i]; i++)
+		len_owner++;
+
+	copy_name = malloc(sizeof(char) * (len_name + 1));
+	if (copy_name == NULL)
+		return (NULL);
+
+	for (i = 0; name[i]; i++)
+		copy_name[i] = name[i];
+	copy_name[i] = '\0';
+
+	copy_owner = malloc(sizeof(char) * (len_owner + 1));
+	if (copy_owner == NULL)
+		return (NULL);
+
+	for (i = 0; owner[i]; i++)
+		copy_owner[i] = owner[i];
+	copy_owner[i] = '\0';
+
+	dog->name = copy_name;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = copy_owner;
 
-	return(dog);
+	return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,6 @@
  * Description: A stucture that contains parameters for the information
  * about each dog.
  */
-
 struct dog
 {
 	char *name;
@@ -20,7 +19,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
