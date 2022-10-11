6a0f87175a8ed5801342ad8ea8a98fd3fa9a54aa
Lincon-snel <linconsnel@gmail.com>
2022-10-11 01:20:10 +0000

    Creating new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,29 +19,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
 	for (i = 0; name[i]; i++)
 		len_name++;
-
 	for (i = 0; owner[i]; i++)
 		len_owner++;
 
 	copy_name = malloc(len_name + 1);
 	if (copy_name == NULL)
+	{
+		free(dog);
 		return (NULL);
-
+	}
 	for (i = 0; name[i]; i++)
 		copy_name[i] = name[i];
 	copy_name[i] = '\0';
 
 	copy_owner = malloc(len_owner + 1);
 	if (copy_owner == NULL)
+	{
+		free(copy_name);
+		free(dog);
 		return (NULL);
-
+	}
 	for (i = 0; owner[i]; i++)
 		copy_owner[i] = owner[i];
 	copy_owner[i] = '\0';
@@ -49,6 +52,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = copy_name;
 	dog->age = age;
 	dog->owner = copy_owner;
-
 	return (dog);
 }
