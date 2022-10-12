bf1519f407e0a9a4d22f99ff0f6d9538c5718a81
Lincon-snel <linconsnel@gmail.com>
2022-10-12 15:18:16 +0000

    Using each element of an array as a parameter to a function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size < 1 || action == NULL)
+	if (array == NULL || size < 1 || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
