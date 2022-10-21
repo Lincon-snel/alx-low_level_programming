ce643b91494dbf223fc882f6e2e3ad94d79f7c31
Lincon-snel <linconsnel@gmail.com>
2022-10-21 02:09:44 +0000

    Prints a string before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first_fun - prints a string before the main function
+ * is executed
+ *
+ * Return: nothing.
+ */
+
+void first_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
