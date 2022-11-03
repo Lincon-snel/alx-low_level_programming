0b23fa2f210d273d11133e5c639fec58ddda1573
Lincon-snel <linconsnel@gmail.com>
2022-11-03 09:58:10 +0000

    Creating files using syscalls

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: pointer to the file to be created.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success,
+ *        -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fdw = write(fdo, text_content, len);
+
+	if (fdo < 0 || fdw < 0)
+		return (-1);
+
+	close(fdo);
+
+	return (1);
+}
